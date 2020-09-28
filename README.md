b6b124069b55b836430cefa106519d278abe94cc
TheHumanGiraffe <vmorganp@protonmail.com>
2020-09-28 02:49:13 -0500

    the og commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Usage
+Make sure you have a working version of docker and git
+
+```
+git clone (the url above)
+cd among-us-discord-bot
+docker build -t stfu .
+docker run stfu
+```
---
+++ index.js
@@ -0,0 +1,82 @@
+// Load up the discord.js library
+const Discord = require("discord.js");
+
+/*
+ DISCORD.JS VERSION 11 CODE
+*/
+
+// This is your client. Some people call it `bot`, some people call it `self`, 
+// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
+// this is what we're refering to. Your client.
+const client = new Discord.Client();
+
+// Here we load the config.json file that contains our token and our prefix values. 
+const config = require("./config.json");
+// config.token contains the bot's token
+// config.prefix contains the message prefix.
+
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started.`);
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setActivity(`Shutting you up`);
+});
+
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("message", async message => {
+  // This event will run on every single message received, from any channel or DM.
+
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
+  if (message.author.bot) return;
+
+  // Also good practice to ignore any message that does not start with our prefix, 
+  // which is set in the configuration file.
+  if (!message.content.startsWith(config.prefix)) return;
+
+  // Here we separate our "command" name, and our "arguments" for the command. 
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  // Let's go with a few common example commands! Feel free to delete or change those.  
+  if (command === "stfu") {
+    roleId = "" // the role you want to mute's id goes here
+    channelId = "" // the channel you want them muted in's id goes here
+
+    role = await message.guild.roles.fetch(roleId)
+
+    channel = await message.guild.channels.resolve(channelId)
+      .updateOverwrite(role, {
+        SPEAK: false
+      })
+  }
+
+  if (command === "unstfu") {
+    roleId = "" // the role you want to mute's id goes here
+    channelId = "" // the chanel you want them muted in's id goes here
+
+    role = await message.guild.roles.fetch(roleId)
+
+    channel = await message.guild.channels.resolve(channelId)
+      .updateOverwrite(role, {
+        SPEAK: true
+      })
+  }
+});
+
+client.login(config.token);
